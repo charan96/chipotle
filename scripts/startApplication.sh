0067d869cdda8bdf2e53e609ef8b17880ff63ffa
charan96 <charan.sudarsanam@gmail.com>
2020-06-17 23:31:57 -0700

    fixing appspec + startApp

--- scripts/startApplication.sh
+++ scripts/startApplication.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-#cd /app
-pwd > /app-start-pwd.txt
+pwd > /app-start-pwd1.txt
+cd /app
+pwd > /app-start-pwd2.txt
 java -jar target/chipotle-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
